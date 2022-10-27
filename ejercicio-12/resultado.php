e5060bdc993198a5306668c44537873d09b47589
JulianH09 <julianhiguita11@gmail.com>
2022-10-27 11:01:57 -0500

    .

--- ejercicio-12/resultado.php
+++ ejercicio-12/resultado.php
@@ -9,13 +9,20 @@
     $resultado1 = $valor * $in2;
     $resultado2 = $valor * $in3;
 
+    $suma1 = $valor + $resultado;
+    $suma2 = $valor + $resultado1;
+    $suma3 = $valor + $resultado2;
+
     echo "Valor registrado " .$valor. "<br>";
 
     if($valor < 500){
-        echo "El interes es de " .$resultado;
+        echo "El interes es de " .$resultado. "<br>";
+        echo "El total a pagar es " .$suma1;    
     }elseif($valor >= 500 && $valor < 1500){
-        echo "El hay interes de " .$resultado1;
+        echo "El hay interes de " .$resultado1. "<br>";
+        echo "El total a pagar es " .$suma2;
     }else{
-        echo "El interes es de " .$resultado2;
+        echo "El interes es de " .$resultado2 ."<br>";
+        echo "El total a pagar es " .$suma3;    
     }
 ?>
